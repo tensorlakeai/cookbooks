a736e935b0da3f657581a0bedf056d1f946f7705
Diptanu Gon Choudhury <diptanuc@gmail.com>
2026-02-11 19:20:51 -0800

    removing unused code

--- oncall-outage-agent/outage_agent/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-On-Call Outage Agent - Powered by Tensorlake
-Automatically diagnoses and fixes production incidents before you wake up.
-"""
-
-from dotenv import load_dotenv
-load_dotenv()
-
-__version__ = "0.1.0"
